5c8506926f9a66f4c0c2c0962b0f018373d9fdcd
Aguesser <wo382408000@hotmail.com>
2024-12-19 21:37:57 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
+  HEO_HERO_TITLE_1: '游戏设计、',
+  HEO_HERO_TITLE_2: '教育与心理',
   HEO_HERO_TITLE_3: 'AGUESSER.COM',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
